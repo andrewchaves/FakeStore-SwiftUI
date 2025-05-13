b9dcf43712849acb25fcd1eb77a63960423fd9cd
Andrew Chaves <andrew.almeidachaves@gmail.com>
2025-05-12 23:41:10 -0400

    Initial commit

---
+++ FakeStore-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FakeStore-SwiftUI
+//
+//  Created by Andrew Vale on 12/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FakeStore-SwiftUI/FakeStore_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStore_SwiftUIApp.swift
+//  FakeStore-SwiftUI
+//
+//  Created by Andrew Vale on 12/05/25.
+//
+
+import SwiftUI
+
+@main
+struct FakeStore_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
