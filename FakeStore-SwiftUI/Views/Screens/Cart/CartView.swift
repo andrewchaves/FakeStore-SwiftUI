5a895a675c358cf0d9763fe3e0218e6e04d353fc
Andrew Chaves <andrew.almeidachaves@gmail.com>
2025-05-31 23:20:38 -0400

    Add Cart List screen stub

---
+++ FakeStore-SwiftUI/Views/Screens/Cart/CartView.swift
@@ -0,0 +1,34 @@
+//
+//  CartView.swift
+//  FakeStore-SwiftUI
+//
+//  Created by Andrew Vale on 31/05/25.
+//
+
+import SwiftUI
+import FakeStoreCore
+struct CartView<ViewModel: CartItemViewModelProtocol & ObservableObject>: View {
+    
+    @ObservedObject private var carItensViewModel: ViewModel
+    
+    init(cartItensViewModel: ViewModel) {
+        self.carItensViewModel = cartItensViewModel
+    }
+    
+    var body: some View {
+        NavigationView {
+            List (carItensViewModel.cartItems){ cartItem in
+                //TODO: - Create Row
+            }
+            .listStyle(.plain)
+            .navigationTitle("FakeStore")
+        }
+        .onAppear {
+            carItensViewModel.fetchCartItems()
+        }
+    }
+}
+
+#Preview {
+    CartView(cartItensViewModel: CartItemVM(cartItemRepository: AppContainer.shared.cartItemRepository))
+}
